83ba5a16486c825c4c622382724d9d3e224f80ed
Mick Smith <micks327@gmail.com>
2025-04-01 14:11:50 -0500

    added function descriptions

--- waterfall.py
+++ waterfall.py
@@ -38,6 +38,15 @@ formulas = Formulas()
 create = PipelineCreate()
 
 def getAssumptionsDF():
+    """
+    Jump across rows to find each new "region" which 
+    contains a datasheet needed for calculation
+
+    Args:
+        None
+    Returns:
+        None 
+    """
     numRowsAssumptions = wsAssumptions.used_range.rows.count
     currentRow = wsAssumptions.range("B1")
 
@@ -48,6 +57,17 @@ def getAssumptionsDF():
             currentRow = currentRow.end('down')
 
 def buildAssumptionsDF(currentRow):
+    """
+    Gets the current region and builds the AssumptionObject class that will house 
+    necessary information of each region. This includes region name, the row the region
+    starts on, a dataframe representing the region data, and whether the data is constant
+
+    Args:
+        currentRow (range object): The range position of the first row of the region
+    Returns:
+        currentRow (range object): Updated range position that is below the last row 
+        of the current region
+    """
     aObj = AssumptionObject()
     currentRegion = currentRow.current_region
 
@@ -75,6 +95,15 @@ def buildAssumptionsDF(currentRow):
 
 
 def GetProductSplitConstant(aObj):
+    """
+    Gets the constant value of the "Product Split" region
+
+    Args:
+        aObj (AssumptionObject class): The object that contains the data for this region
+    Returns:
+        wsRange (range object): the range object that represents the address of the 
+        constant value
+    """
     filtered_df = aObj.df
 
     for i in range(0, len(dimensionsTitles)-1):
@@ -87,6 +116,15 @@ def GetProductSplitConstant(aObj):
 
     
 def GetSalesCycleConstants(aObj):
+    """
+    Gets the constant values of the "Sales Cycle" region
+
+    Args:
+        aObj (AssumptionObject class): The object that contains the data for this region
+    Returns:
+        wsRange (range object): the range object that represents the addresses of the 
+        constant values
+    """
     filtered_df = aObj.df
 
     for i in range(0, len(dimensionsTitles)-1):
@@ -100,6 +138,14 @@ def GetSalesCycleConstants(aObj):
     return row
 
 def cycleRegions():
+    """
+    Cycles through each AssumptionObject within the AssumptionRegion class' region list
+
+    Args:
+        None
+    Returns:
+        None
+    """
     for i in range(0, len(aRegion.region)):
         aObj = aRegion.region[i]
 
@@ -108,6 +154,15 @@ def cycleRegions():
     return
 
 def parseRegion(aObj):
+    """
+    Parses through the data in the AssumptionObject's dataframe, removing any rows that do not
+    match with the expected dimensions from the Output worksheet.
+
+    Args:
+        aObj (AssumptionObject class): The object that contains the data for a particular region
+    Returns:
+        None
+    """
     filtered_df = aObj.df
 
     for i in range(0, len(dimensionsTitles)-1):
@@ -118,6 +173,17 @@ def parseRegion(aObj):
     return
 
 def runThroughDates():
+    """
+    Runs through each date from the associated column titles in the Output worksheet.
+    Creates the "refMatrix" list that stores the range addresses for each value of each 
+    region for the associated date.
+     
+    Args:
+        None
+    Returns:
+        refMatrix (multidimensional list): The list that contains that values from each 
+        region for each date.
+    """
     refMatrix = []
     for i in range(0, len(dateValues)):
         currentDate = dateValues[i]
@@ -130,6 +196,17 @@ def runThroughDates():
     return refMatrix
 
 def getCellReferenceForDate(currentDate):
+    """
+    Loops through each region and gets the range address for each value associated 
+    with that date
+     
+    Args:
+        currentDate (date object): The date being used to get the range address for 
+        each value
+    Returns:
+        refArray (list): The list that contains that values from each 
+        region for each date.
+    """
     refArray = [None] * 6
     for j in range(0, len(aRegion.region)):
         aObj = aRegion.region[j]
@@ -145,6 +222,18 @@ def getCellReferenceForDate(currentDate):
     return refArray
 
 def getCellReference(dateVal, aObj):
+    """
+    Gets the range object from the filtered dataframe from the associated
+    AssumptionObject using the date value.
+     
+    Args:
+        dateVal (date object | string): The date to be searched
+        aObj (AssumptionObject class): The region object whose dataframe is used to 
+        retrive the range object for the specified date.
+    Returns:
+        wsRange (range object): The range object of the associated value for the 
+        specified date.
+    """
     date_df = aObj.filtered_df.loc[aObj.filtered_df['Timeframe'] == dateVal]
 
     row = date_df.index.to_list()
@@ -154,6 +243,15 @@ def getCellReference(dateVal, aObj):
     return wsRange
 
 def convertDateToQuarter(currentDate):
+    """
+    Converts the date object to a string that represents the financial quarter and year
+    (01/01/2025) => 2025-Q1
+     
+    Args:
+        currentDate (date object): The date to be converted
+    Returns:
+        quarterNum (string): The quarter and year from the given currentDate
+    """
     yearNum = currentDate.year
     monthNum = currentDate.month
 
@@ -169,6 +267,22 @@ def convertDateToQuarter(currentDate):
     return quarterNum
 
 def buildCellCatalogue(refMatrix, salesCycleConsts, prodSplitConst):
+    """
+    Builds the cell catalogue. The cell catalogue is a multi-dimensional list where
+    each value stored is a string that represents a single formula for the given dimensions
+    in the Output workSheet. 
+     
+    Args:
+        refMatrix (multidimensional list): The list that contains that values from each 
+        region for each date.
+        salesCycleConsts (range object): The range objects for the const values in the 
+        "Sales Cycle" region from "in month" to "24 months".
+        prodSplitConst (range object): The range object for the const value in the 
+        "Product Split" region.
+    Returns:
+        catalogue (multi-dimensional list): The single formula for each combination of region
+        value addresses for each date.
+    """
     catalogue = [([None] * len(salesCycleConsts)) for _ in range(len(refMatrix))]
 
     print(refMatrix[0][0] + '*' + refMatrix[0][1])
@@ -186,6 +300,16 @@ def buildCellCatalogue(refMatrix, salesCycleConsts, prodSplitConst):
     return catalogue
 
 def builFormulas(catalogue):
+    """
+    Cycles through each sublist in the catalogue multi-dimensional list. This sublist is
+    represented by the variable "cellRef".
+     
+    Args:
+        catalogue (multi-dimensional list): The single formula for each combination of region
+        value addresses for each date.
+    Returns:
+        None
+    """
     maxval = len(dateValues)
     for x in range(0, len(catalogue)):
         cellRef = catalogue[x]
@@ -194,6 +318,28 @@ def builFormulas(catalogue):
     return
 
 def cycleCatalogue(cellRef, colStart, maxCol):
+    """
+    Loops through each cellRef sublist from the catalogue multi-dimensional list. Creates 
+    variables "colNum" and "arrayOffset" needed for correctly building the waterfall. 
+
+    |   Date 1   |   Date 2                | ...
+    ---------------------------------------
+    | cellRef 1  | cellRef 2  + cellRef 1a | ...
+    ---------------------------------------
+    | cellRef 1a | cellRef 2a + cellRef 1b | ...
+    ---------------------------------------
+    | cellRef 1b | cellRef 2b + ...        | 
+     
+    Args:
+        cellRef (list): The sublist of the catalogue that contains the single formula for
+        date and dimension.
+        colStart (integer): The starting column for the respective date column in the 
+        Output worksheet
+        maxCol: The maximum column value to be used, in this case it's the column representing
+        december 2026.
+    Returns:
+        None
+    """
     for y in range(0, len(cellRef)):
         arrayOffset = y
         colNum = colStart + y
@@ -205,6 +351,26 @@ def cycleCatalogue(cellRef, colStart, maxCol):
     return
 
 def insertReferences(cellRef, colNum, arrayOffset):
+    """
+    Goes down each column row (y) for the respective dates (colNum) and adds the formula string
+    to the previous formula. These are stored in the PipelineCreate object "formulas.matrix"
+
+           | colNum =  1 | colNum =  2     | ...
+    -------|------------------------------------
+     y = 0 | cellRef 1   | cellRef 1a + ...| ...
+    -------|------------------------------------
+     y = 1 | cellRef 1a  | cellRef 1b + ...| ...
+    -------|------------------------------------
+     y = 2 | cellRef 1b  | cellRef 1c + ...| 
+     
+    Args:
+        cellRef (list): The sublist of the catalogue that contains the single formula for
+        each date and dimension.
+        colNum (integer): The column offset from the starting column
+        maxCol: The offset value to start inserting the values from the cellRef list.
+    Returns:
+        None
+    """
     for z in range(0, 13):
         if z + arrayOffset > len(cellRef)-1:
             break
@@ -218,13 +384,30 @@ def insertReferences(cellRef, colNum, arrayOffset):
     return
 
 def BuildWaterfall():
+    """
+    Inserts the full formula from formulas.matrix into the corresponding cell in 
+    the output worksheet.
+     
+    Args:
+        None
+    Returns:
+        None
+    """
     for x in range(0, len(dateValues)):
         for y in range(0, 13):
             wsOutput.range((3+y,9+x)).value = '=' + formulas.matrix[x][y]
-            
     return
 
 def BuildPipelineCreate():
+    """
+    Inserts the pipeline create formulas (headcount x productivity) the corresponds
+    to the dimensions for that date.
+     
+    Args:
+        None
+    Returns:
+        None
+    """
     for x in range(0, len(create.values)):
         wsOutput.range((17,9+x)).color = (11, 48, 64)
         wsOutput.range((17,9+x)).value = 'Create'
